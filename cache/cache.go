a61baa23b1bd3f407acdc23974a48395b5a6b21f
guoxiaoshuang <guoxiaoshuang@zhihu.com>
2021-03-19 00:38:54 +0800

    init cache

---
+++ cache/cache.go
@@ -0,0 +1,26 @@
+package cache
+
+type Cache interface {
+	Set(string, []byte)
+	Get(string) ([]byte, error)
+	Del(string) error
+	GetStat() Stat
+}
+
+type Stat struct {
+	Count     int64
+	KeySize   int64
+	ValueSize int64
+}
+
+func (s *Stat) Add(k string, v []byte) {
+	s.Count += 1
+	s.KeySize += int64(len(k))
+	s.ValueSize += int64(len(v))
+}
+
+func (s *Stat) Del(k string, v []byte){
+	s.Count -= 1
+	s.KeySize -= int64(len(k))
+	s.ValueSize -= int64(len(v))
+}
---
+++ cache/impl/memory_cache.go
@@ -0,0 +1,52 @@
+package impl
+
+import (
+	"distributed-cache/cache"
+	"fmt"
+	"sync"
+)
+
+type MemoryCache struct {
+	cache   map[string][]byte
+	rwMutex sync.RWMutex
+	cache.Stat
+}
+
+func (m *MemoryCache) Set(k string, v []byte) {
+	m.rwMutex.Lock()
+	defer m.rwMutex.Unlock()
+	if _, ok := m.cache[k]; ok {
+		m.Stat.Del(k, v)
+	}
+	m.cache[k] = v
+	m.Stat.Add(k, v)
+}
+
+func (m *MemoryCache) Get(k string) ([]byte, error) {
+	m.rwMutex.RLock()
+	defer m.rwMutex.RUnlock()
+	if v, ok := m.cache[k]; ok {
+		return v, nil
+	}
+	return nil, fmt.Errorf("this key not exists")
+}
+
+func (m *MemoryCache) Del(k string) error {
+	m.rwMutex.Lock()
+	defer m.rwMutex.Unlock()
+	if v, ok := m.cache[k]; ok {
+		m.Stat.Del(k, v)
+		delete(m.cache, k)
+		return nil
+	}
+	return fmt.Errorf("this key not exists")
+}
+
+func (m *MemoryCache) GetStat() cache.Stat {
+	return m.Stat
+}
+
+
+func NewMemoryCache()*MemoryCache{
+	return &MemoryCache{cache:make(map[string][]byte), rwMutex: sync.RWMutex{}, Stat: cache.Stat{}}
+}
